41af9e6f3cad7409bd2e07df7c405a0e578a50c9
Paul Okello <paulokello273@gmail.com>
2021-04-12 12:15:04 +0300

    product_detail mvc

--- shop/urls.py
+++ shop/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('<slug:category_slug>/',
          views.product_list,
          name='product_list_by_category'),
-    path('<slug:category_slug>/', views.product_detail, name='product_detail'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
