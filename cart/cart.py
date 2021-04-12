4b1ac9e238bf4ccd0be24a86d602209715d77c82
Paul Okello <paulokello273@gmail.com>
2021-04-12 14:05:50 +0300

    Cart class with cart functionality

--- cart/cart.py
+++ cart/cart.py
@@ -7,11 +7,11 @@ class Cart(object):
     def __init__(self, request):
         #Initialize the cart
         self.session = request.session
-        cart = self.session.get(settings.CArt_SESSION_ID)
+        cart = self.session.get(settings.CART_SESSION_ID)
 
         if not cart:
             #Save an empty cart in the session
-            cart = self.session[settings.CART_SESSION_ID] = ()
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
@@ -62,7 +62,7 @@ class Cart(object):
 
     def get_total_price(self):
         return sum(
-            Deciaml(item['price']) * item['quantity']
+            Decimal(item['price']) * item['quantity']
             for item in self.cart.values())
 
     def clear(self):
